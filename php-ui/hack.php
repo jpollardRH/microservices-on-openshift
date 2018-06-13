7fe86661d73eafaae38849f8e8a45f9b2fa1874d
Jamie Pollard <36760590+jpollardRH@users.noreply.github.com>
2018-06-13 13:39:27 +0200

    Updated hack.php
    Amended hack.php to include column names in SQL statement.

--- php-ui/hack.php
+++ php-ui/hack.php
@@ -6,7 +6,7 @@ $dbname = "microservices";
 $dbpwd = "password";
  
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
-$query = "SELECT * from emails" or die("Error in the consult.." . mysqli_error($connection));
+$query = "SELECT from_add, to_add, subject, body, created_at from emails" or die("Error in the consult.." . mysqli_error($connection));
 echo "Here is the list of emails sent: <br>";
 $rs = $connection->query($query);
 while ($row = mysqli_fetch_assoc($rs)) {
