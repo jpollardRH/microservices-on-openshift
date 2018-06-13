68ba0cf574346f8b8a756fba674a1b6b79272dda
Jamie Pollard <36760590+jpollardRH@users.noreply.github.com>
2018-06-13 13:54:23 +0200

    Update hack.php
    Removed problem with empty resultset.

--- php-ui/hack.php
+++ php-ui/hack.php
@@ -1,16 +1,18 @@
 <?php
-$dbhost = gethostbyname('mysql.msinfra'); 
+$dbhost = gethostbyname('mysql.msinfra');
 $dbport = 3306;
 $dbuser = "app_user";
 $dbname = "microservices";
 $dbpwd = "password";
- 
+
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
-$query = "SELECT from_add, to_add, subject, body, created_at from emails" or die("Error in the consult.." . mysqli_error($connection));
+$query = "SELECT from_add, to_add, subject, body, created_at from emails";
 echo "Here is the list of emails sent: <br>";
-$rs = $connection->query($query);
-while ($row = mysqli_fetch_assoc($rs)) {
+if ($result = mysqli_query($connection, $query)) {
+while ($row = mysqli_fetch_assoc($result)) {
     echo "From Address: ".$row['from_add'] . "To Address: " . $row['to_add'] . "Subject: " . $row['subject'] ."When: ".$row['created_at'] . "<br>";
+}
+mysqli_free_result($result);
 }
 echo "End of the list <br>";
 mysqli_close($connection);
